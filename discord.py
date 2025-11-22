2c339f1a900aae277a4d873621aae3c5b63d5f9e
posobiak-star <posobiak@gmail.com>
2025-11-22 16:33:33 +0900

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,1 +1,24 @@
+# bot.py
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = commands.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot logged in as: {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+bot.run(TOKEN)
 
