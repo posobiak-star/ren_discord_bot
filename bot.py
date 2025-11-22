db6386017fdb3c9c0d3dc062113f1aa760b95652
posobiak-star <posobiak@gmail.com>
2025-11-22 16:54:52 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-# bot.py
+import os
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default()
-
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents.message_content = True
+intents = discord.Intents.default()
+intents.message_content = True  # メッセージ内容にアクセスする場合は必須
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
